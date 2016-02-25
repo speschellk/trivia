56d4604837b45005341ad767ab992d1f26186372
karinamk <karinakocemba@gmail.com>
2016-02-25 18:59:21 -0500

    Update main.js
    -added nav bar interactivity
    -added hide on category pictures
    -next step is to update html file so that there are consistent classes for specifics in the content and categories sections in order to do comparison (i.e., show if class of content clicked === class of category)

--- main.js
+++ main.js
@@ -2,31 +2,49 @@ console.log('hello world');
 
 $(document).ready(function(){
 
-/////PAGE NAVIGATION////////
-//add event listener to make nav actually navigate
-//create an click event listener
-//do this when someone clicks on a link 
- //in the nav bar
-    $("nav a").click(function(){
-      //hide all article elements
-      $("#main-content").hide();
-      //provide value for clicked element's href attribute
-      var id = $(this).attr("href");
-      //show 
-	  $(id).show();
-	  $(id).slideDown();
-      $(this).css("color","blue");
-    }
 
-});
+//hide all page content elements other than main
+//$("#main").show();
+$("#categories").show();
+$("#main-content").hide();
+$("#trivia-basics").hide();
+$("#about").hide();
+$("#contact").hide();
 
+  /////PAGE NAVIGATION////////
+  //add event listener to make nav actually navigate
+  //create an click event listener
+  //do this when someone clicks on a link 
+  //in the nav bar
+  $("nav a").click(function(){
+     //hide all content again
+     $("#categories").hide(); 
+     $("#main-content").hide();
+     $("#trivia-basics").hide();
+     $("#about").hide();
+     $("#contact").hide();  
+    //provide value for clicked element's href attribute
+    var id = $(this).attr("href");
+    //show the element with the id that was clicked
+  	$(id).show();
+  	$(id).fadeIn();
+    $(this).css("color","grey");
+  })
 
-// add links to each category that open content on that page and hide content home page
-	$("div").click(
-  function(){ 
-	  //hide main context section on click of div element
-		$("#main-content").hide();
-  //show the element with id of what was clicked 
-	}
-  )
+  /////CATEGORY NAVIGATION////////
+  // add links to each category that open content on that page
+  // and hide content home page
+	$("div.trivia-category").click(function(){ 
+	  console.log('category button clicked');
+    //hide main cateogry buttons on click of a category div element
+		$("#categories").hide();
+    //provide value for clicked element's id
+    var c = $(this).attr('class').split(' ')[1];
+    console.log('clicked second class is ' + c)
+    //show the content element with class of what was clicked
+    // TODO: add if statement comparing if c===[second class of content]
+      //$(c).show();
+
+  });
+
 });
