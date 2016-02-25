241446f180b22c7a10ccde5efa8f9d63097f8af4
karinamk <karinakocemba@gmail.com>
2016-02-24 20:49:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-jQuery(function(){
+console.log('hello world');
+
+$(document).ready(function(){
+
+/////PAGE NAVIGATION////////
+//add event listener to make nav actually navigate
+//create an click event listener
+//do this when someone clicks on a link 
+ //in the nav bar
+    $("nav a").click(function(){
+      //hide all article elements
+      $("#main-content").hide();
+      //provide value for clicked element's href attribute
+      var id = $(this).attr("href");
+      //show 
+	  $(id).show();
+	  $(id).slideDown();
+      $(this).css("color","blue");
+    }
+
+});
+
 
 // add links to each category that open content on that page and hide content home page
-	
 	$("div").click(
   function(){ 
 	  //hide main context section on click of div element
 		$("#main-content").hide();
   //show the element with id of what was clicked 
-  	 	var id = $(this).attr("id");
-		$(id).show();
 	}
   )
 });
