ec092de4951945f3586b9a6640cb423244df5cf7
karinamk <karinakocemba@gmail.com>
2016-03-14 19:41:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-var channelName = 'SportsNews';
+var channelName = 'Sports';
 var vidWidth = '500';
 var vidHeight = '400';
 var vidResults = 10;
-//var channels = ["#Education", "#Sports", "Sports News", "#News", "Entertainment News"]
-//var Playlists = ["That's a Good Question",]
+var channels = ["Education", "Sports", "Sports News", "News", "Entertainment News"]
+var Playlists = ["That's a Good Question",]
 
 $(document).ready(function(){
 
 	console.log('hello world');
+	$("#contact").hide();
 //hide all page content elements other than main
 //$("#main").show();
 /*$("#categories").show();
 $("#main-content").hide();
 $("#trivia-basics").hide();
 $("#about").hide();
-$("#contact").hide();*/
+
   /////PAGE NAVIGATION////////
   //add event listener to make nav actually navigate
   //create an click event listener
@@ -71,6 +72,7 @@ $("#contact").hide();*/
  	$.get("https://www.googleapis.com/youtube/v3/channels", {
 	 		part: 'contentDetails',
 			forUsername: channelName,
+			//topicId: ,
 			key: 'AIzaSyCLowtBqq3Oo7jG0DJWvZwcNf3hmNM0n4A'},
 			function(data) {
 				$.each(data.items, function(i, item){
@@ -84,7 +86,7 @@ $("#contact").hide();*/
 	function getVids(pid){
  	$.get(
 		"https://www.googleapis.com/youtube/v3/playlistItems", {
-	 		part: 'snippet',
+	 		part: 'snippet' ,
 			maxResults: vidResults,
 			playlistId: pid,	
 			key: 'AIzaSyCLowtBqq3Oo7jG0DJWvZwcNf3hmNM0n4A'},
@@ -94,8 +96,8 @@ $("#contact").hide();*/
 					console.log(item);
 					vidTitle= item.snippet.title;
 					videoId = item.snippet.resourceId.videoId;
-					output = '<li>'+vidTitle+'</li>';
-					//output = '<li><iframe height="+vidHeight+" width="+vidWidth+" src=\"//www.youtube.com/embed/'+videoId+'"\></iframe></li>';
+					//output = '<li>'+vidTitle+'</li>';
+					output = '<li><iframe height="+vidHeight+" width="+vidWidth+" src=\"//www.youtube.com/embed/'+videoId+'"\></iframe></li>';
 					//Append to results listStyleType
 					$('#results').append(output);
 				})
